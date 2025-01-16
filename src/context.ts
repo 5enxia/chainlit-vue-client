d78b52d8065fd57f0a17348d17e8b9f901e50c1f
5enxia <takayasumorishita@gmail.com>
2025-01-16 18:28:07 +0900

    feat: implement createChainlit function for context installation

--- src/context.ts
+++ src/context.ts
@@ -1,21 +1,30 @@
-import { provide, inject, type InjectionKey } from 'vue';
-import { ChainlitAPI } from './api';
+import { inject, type InjectionKey, type App } from "vue";
+import { ChainlitAPI } from "./api";
 
 const defaultChainlitContext: ChainlitAPI | undefined = undefined;
 
-const ChainlitContextSymbol: InjectionKey<ChainlitAPI> = Symbol('ChainlitContext');
+const ChainlitContextSymbol: InjectionKey<ChainlitAPI> =
+  Symbol("ChainlitContext");
+export interface Chainlit {
+  install: (app: App) => void;
+}
 
-// export function provideChainlitContext() {
-//   const chainlitContext = new ChainlitAPI('http://localhost:8000', 'webapp');
-//   provide(ChainlitContextSymbol, chainlitContext);
-// }
+export function createChainlit(api: ChainlitAPI): Chainlit {
+  return {
+    install(app: App) {
+      app.provide(ChainlitContextSymbol, api);
+    },
+  };
+}
 
-// export function useChainlitContext() {
-//   const context = inject(ChainlitContextSymbol, defaultChainlitContext);
-//   if (!context) {
-//     throw new Error('useChainlitContext must be used within a provideChainlitContext');
-//   }
-//   return context;
-// }
+export function useChainlitContext() {
+  const context = inject(ChainlitContextSymbol, defaultChainlitContext);
+  if (!context) {
+    throw new Error(
+      "useChainlitContext must be used within a provideChainlitContext"
+    );
+  }
+  return context;
+}
 
 export { ChainlitContextSymbol };
