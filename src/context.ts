5c4f0f8197053f3b1572fde9659f4437c57f4c0a
5enxia <takayasumorishita@gmail.com>
2025-01-19 18:04:56 +0900

    feat: update Chainlit install method to require Pinia instance for state management

--- src/context.ts
+++ src/context.ts
@@ -1,19 +1,34 @@
 import { inject, type InjectionKey, type App } from "vue";
 
 import { ChainlitAPI } from "./api";
+import type { Pinia } from "pinia";
+import { useStateStore } from "./state";
 
 const defaultChainlitContext: ChainlitAPI | undefined = undefined;
 
 const ChainlitContextSymbol: InjectionKey<ChainlitAPI> =
   Symbol("ChainlitContext");
+
+interface Options{
+  pinia: Pinia;
+}
 export interface Chainlit {
-  install: (app: App) => void;
+  install: (app: App, options: Options) => void;
 }
 
+// https://stackoverflow.com/questions/71691842/how-to-use-pinia-inside-of-an-npm-package
 export const createChainlit = (api: ChainlitAPI): Chainlit => {
   return {
-    install(app: App) {
+    install(app: App, options: Options) {
       app.provide(ChainlitContextSymbol, api);
+
+      const { pinia } = options;
+
+      if (!pinia) {
+        throw new Error("Pinia instance is required");
+      }
+
+      useStateStore(pinia);
     },
   };
 };
